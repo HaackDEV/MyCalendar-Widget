11fe916fbd3faed9e93a03f439c963b636d8444f
Ruan Haack <ruan.haack03@gmail.com>
2026-05-08 00:16:56 -0300

    feat: add functions to filter events upcoming by date

--- index.js
+++ index.js
@@ -18,10 +18,75 @@ async function readConfig() {
 // Download .ics file.
 async function downloadIcsFile(url) {
     try {
-        const calendarDate = ical.async.fromURL(url);
+        const calendarDate = await ical.async.fromURL(url);
         return calendarDate;
     } catch (error) {
         console.error('Error: Cannot download this file:', error);
+    }
+}
+
+// Filter events on current day and return an array.
+function filterTodayEvents(calendarDate) {
+    try {
+        const today = new Date;
+
+        // Create today bound (00:00:00 até 23:59:59).
+        const startToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+        const endToday = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59, 999);
+
+        const events = Object.values(calendarDate).filter(item => item.type === 'VEVENT');
+
+        return events.filter(event => {
+            // Check if isn't a recurring event.
+            if (event.start && !event.rrule) {
+                const eventDate = new Date (event.start);
+                return eventDate >= startToday && eventDate <= endToday;
+            }
+            // Check if is a recurring event.
+            if (event.rrule){
+                 const occurrences = event.rrule.between(startToday, endToday, true);
+                 return occurrences.length > 0;
+            }
+
+            return false;
+    })
+    } catch (error) {
+        console.error('Error: Cannot find any event', error);
+        return [];
+    }
+}
+
+// Filter events on current week and return an array.
+function filterWeekEvents(calendarDate){
+    try {
+        const today = new Date;
+
+        // Calculate week bound (start: today + 1 day, end: +7 days)
+        const startWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+        const endWeek = new Date(startWeek);
+        endWeek.setDate(startWeek.getDate() + 7);
+        endWeek.setHours(23, 59, 59, 999);
+
+        const events = Object.values(calendarDate).filter(item => item.type === 'VEVENT');
+
+        return events.filter(event => {
+            // Check if isn't a recurring event.
+            if (event.start && !event.rrule) {
+                const eventDate = new Date (event.start);
+                return eventDate >= startWeek && eventDate <= endWeek;
+            }
+            
+            // Check if it's a recurring event.
+            if (event.rrule){
+                 const occurrences = event.rrule.between(startWeek, endWeek, true);
+                 return occurrences.length > 0;
+            }
+
+            return false;
+        })
+    } catch (error) {
+        console.error('Error: Cannot find any event', error);
+        return [];
     }
 }
 
@@ -32,6 +97,11 @@ async function main() {
     console.log(config?.ical_url);
     const calendarDate = await downloadIcsFile(ical_url);
     console.log(Object.keys(calendarDate).length);
+    const weekEvents = filterWeekEvents(calendarDate)
+    console.log(weekEvents);
+    const todayEvents = filterTodayEvents(calendarDate);
+    console.log(todayEvents);
 }
 
 main();
+
